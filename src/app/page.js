f33a1c73a78e7a7c0b91349b6aee726b281429a8
Ritu Raj <145446035+riturajroman@users.noreply.github.com>
2024-08-23 18:15:44 +0530

    first change

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Styles from "./page.module.css";
+
+function HomePage() {
+  return (
+    <div className={Styles.page}>
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+export default HomePage;
